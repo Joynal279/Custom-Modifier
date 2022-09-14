63700cb16daf036c2448a2432548a27ef94a40d9
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-14 08:57:04 +0600

    Initial Commit

---
+++ Custom Modifier/Custom_ModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_ModifierApp.swift
+//  Custom Modifier
+//
+//  Created by JOYNAL ABEDIN on 14/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Custom_ModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
